605cf397ea47e4be2fee0369d8a6709309298eaf
Valery Fremaux <valery.fremaux@gmail.com>
2022-11-22 11:04:03 +0100

    %1

--- version.php
+++ version.php
@@ -27,11 +27,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016052400;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2019111200;        // Requires this Moodle version.
+$plugin->requires  = 2022041200;        // Requires this Moodle version.
 $plugin->component = 'block_vmoodle'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "3.8.0 (Build 2016052400)";
+$plugin->release = "4.0.0 (Build 2016052400)";
 $plugin->dependencies = array('local_vmoodle' => 2017090101);
+$plugin->supported = [40,40];
 
 // Not moodle attributes.
-$plugin->codeincrement = '3.8.0000';
+$plugin->codeincrement = '4.0.0000';
